0f7291afd89630e827d5213682cb747be97c6aab
Lynette Cargo <lynettecargo@gmail.com>
2023-08-17 14:06:33 -0500

    CRUD app

--- server.js
+++ server.js
@@ -22,29 +22,52 @@ app.use(express.json({ extended: false }));
 // main page
 app.get("/", async(req,res) => {
     // res.send('HELLO👋🏼')
-    // ************************INDEX ROUTE***
+    // ********************************** INDEX ROUTE***
     try {
         const allPosts = await Post.find({});
         res.send(allPosts)
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error")
+        res.status(500).send("Read Route Server Error")
     }
 })
+
+// ************************************** DELETE
+app.delete('/:id', async(req,res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id)
+        res.send('Post Deleted')
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Delete Server Error')
+    }
+})
+
+// ************************************** UPDATE
+// UPDATE ROUTE CR[U]D
+app.put('/:id', async(req,res) => {
+    try {
+        // {new:true} helps us output the new data as opposed to old data
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.send(post)
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Update Server Error")
+    }
+})
+
 
 // create route [C]RUD
 app.post('/', async(req,res) => {
     try {
        const post = await Post.create(req.body)
-       res.send(post)
+       res.send(post) // this will have an _id
     } catch (err) {
-        console.error(err);
-        // console.error(err.message)
-        // res.status(500).send("Server Error")
+        // console.error(err);
+        console.error(err.message)
+        res.status(500).send("Server Error")
     }
-    // res.send(post) // This will have an _id
 })
-
 
 
 // ************************************************* SERVER
